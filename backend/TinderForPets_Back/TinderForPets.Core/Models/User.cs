13c22a8615bb3885a368a6d21ab1adac27350a33
mnek1t <mednikita2004@gmail.com>
2024-10-02 01:23:05 +0300

    domain layer

---
+++ backend/TinderForPets_Back/TinderForPets.Core/Models/User.cs
@@ -0,0 +1,28 @@
+﻿namespace TinderForPets.Core.Models
+{
+    public class User
+    {
+        private User(Guid id, string userName, string passwordHash, string email)
+        {
+            Id = id;
+            UserName = userName;
+            PasswordHash = passwordHash;
+            Email = email;
+        }
+        public Guid Id { get; set; }
+        public string UserName { get; private set; }
+        public string PasswordHash { get; private set; }
+        public string Email { get; private set; }
+
+        public static User Create(Guid id, string userName, string passwordHash, string email)
+        {
+            var error = string.Empty;
+            if (string.IsNullOrEmpty(userName) ) 
+            {
+
+            }
+
+            return new User(id, userName, passwordHash, email);
+        }
+    }
+}
