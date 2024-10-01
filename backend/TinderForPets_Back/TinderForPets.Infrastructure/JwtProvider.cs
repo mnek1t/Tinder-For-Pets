eaad94ba4dbc0eff97521e707b0cefc7558f9829
mnek1t <mednikita2004@gmail.com>
2024-10-02 01:20:57 +0300

    adding generating JWT token functional

---
+++ backend/TinderForPets_Back/TinderForPets.Infrastructure/JwtProvider.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using TinderForPets.Core.Models;
+
+namespace TinderForPets.Infrastructure
+{
+    public class JwtProvider : IJwtProvider
+    {
+        private readonly JwtOptions _options;
+        public JwtProvider(IOptions<JwtOptions> options)
+        {
+            _options = options.Value;
+        }
+        //TODO: USer Type is not from TinderForPets.API 
+        public string GenerateToken(User user)
+        {
+            Claim[] claims = [new("userId", user.Id.ToString())];
+
+            var signingCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(_options.SecretKey)),
+                    SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                signingCredentials: signingCredentials,
+                expires: DateTime.UtcNow.AddHours(_options.ExpiresHours)
+            );
+
+            var tokenValue = new JwtSecurityTokenHandler().WriteToken(token);
+            return tokenValue;
+        }
+    }
+}
