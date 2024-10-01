6a459c688ed57a02cfbc2950b55757c34f3f0f5e
mnek1t <mednikita2004@gmail.com>
2024-10-02 01:18:17 +0300

    implementing user repository

---
+++ backend/TinderForPets_Back/TinderForPets.Data/Interfaces/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using TinderForPets.Core.Models;
+using SharedKernel;
+
+namespace TinderForPets.Data.Interfaces
+{
+    public interface IUserRepository
+    {   
+        Task<Guid> Add(User user);
+        Task<Result<User>> GetByEmail(string email); 
+    }
+}
---
+++ backend/TinderForPets_Back/TinderForPets.Data/Repositories/UserRepository.cs
@@ -0,0 +1,73 @@
+﻿using TinderForPets.Data.Interfaces;
+using TinderForPets.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using TinderForPets.Data.Entities;
+using TinderForPets.Data.Exceptions;
+using SharedKernel;
+using TinderForPets.Core;
+using AutoMapper;
+
+namespace TinderForPets.Data.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly TinderForPetsDbContext _context;
+        private readonly IMapper _mapper;
+
+        public UserRepository(TinderForPetsDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<Guid> Add(User user)
+        {
+            var userEntity = _mapper.Map<UserAccount>(user);
+
+            await _context.AddAsync(userEntity);
+            await _context.SaveChangesAsync();
+            return userEntity.Id;
+        }
+
+        public async Task Update(User user)
+        {
+            // TODO: complete 
+            if (user == null) 
+            {
+                throw new Exception("User is null");
+            }
+
+            await _context.UserAccounts
+                .Where(u => u.Id == user.Id)
+                .ExecuteUpdateAsync(u => u
+                    .SetProperty(u => u.EmailAddress, user.Email)
+                    .SetProperty(u => u.UserName, user.UserName)
+                    .SetProperty(u => u.Password, user.PasswordHash));
+        }
+
+        public async Task<Result<User>> GetByEmail(string email)
+        {
+            var userEntity = await _context.UserAccounts
+                .AsNoTracking()
+                .FirstOrDefaultAsync(u => u.EmailAddress == email);
+            if (userEntity == null) 
+            {
+                return Result.Failure<User>(UserErrors.NotFoundByEmail(email));
+            }
+
+            var user = _mapper.Map<User>(userEntity);
+
+            return Result.Success<User>(user);
+        }
+
+        public async Task<List<User>> GetUsers()
+        {
+            // TODO: complete 
+            var userEntities = await _context.UserAccounts.AsNoTracking().ToListAsync();
+
+            // TODO: Create User type defined in Core from Entity. Use Mapper!   
+
+            throw new NotImplementedException();
+        }
+    }
+}
