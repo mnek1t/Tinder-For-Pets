794c1127ab54fe892c1846e6741c4c6452c2a932
mnek1t <mednikita2004@gmail.com>
2024-10-02 01:20:36 +0300

    hashing password

---
+++ backend/TinderForPets_Back/TinderForPets.Infrastructure/PasswordHasher.cs
@@ -0,0 +1,16 @@
+﻿using BCrypt.Net;
+namespace TinderForPets.Infrastructure
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        public string Generate(string password)
+        {
+            return BCrypt.Net.BCrypt.EnhancedHashPassword(password);
+        }
+
+        public bool Verify(string password, string hashedPassword)
+        {
+            return BCrypt.Net.BCrypt.EnhancedVerify(password, hashedPassword);
+        }
+    }
+}
