8919534493c22725af14dd6c5c03ed9ff6b983ba
mnek1t <mednikita2004@gmail.com>
2024-10-02 01:22:41 +0300

    email validation

---
+++ backend/TinderForPets_Back/TinderForPets.Infrastructure/EmailHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+namespace TinderForPets.Infrastructure
+{
+    public partial class EmailHandler
+    {
+        private const string emailPattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+        public static bool IsValidEmailFormat(string email)
+        {
+            return MyRegex().IsMatch(email);
+        }
+
+        [GeneratedRegex(emailPattern)]
+        private static partial Regex MyRegex();
+    }
+}
