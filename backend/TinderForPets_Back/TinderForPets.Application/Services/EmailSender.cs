43e6d258e98a37ad5a906fa26ed0da8f7c9df60e
mnek1t <mednikita2004@gmail.com>
2024-10-07 15:18:51 +0300

    sending email for forgot password request

---
+++ backend/TinderForPets_Back/TinderForPets.Application/Services/EmailSender.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Text.Json;
+using TinderForPets.Infrastructure;
+
+namespace TinderForPets.Application.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly ILogger _logger;
+
+        public EmailSender(IOptions<AuthMessageSenderOptions> optionsAccessor,
+                           ILogger<EmailSender> logger)
+        {
+            Options = optionsAccessor.Value;
+            _logger = logger;
+        }
+
+        public AuthMessageSenderOptions Options { get; }
+
+        public async Task SendEmailAsync(string toEmail, string subject, string message)
+        {
+
+            if (string.IsNullOrEmpty(Options.SendGridKey))
+            {
+                throw new Exception("Null SendGridKey");
+            }
+            await Execute(Options.SendGridKey, subject, message, toEmail);
+        }
+
+        public async Task Execute(string apiKey, string subject, string message, string toEmail)
+        {
+            var client = new SendGridClient(apiKey);
+            var templatePath = Path.Combine(Directory.GetCurrentDirectory(), "EmailTemplates", "reset-password-template.html");
+            var htmlMessage = await File.ReadAllTextAsync(templatePath);
+            var emailData = JsonSerializer.Deserialize<ResetPasswordEmailDto>(message);
+            htmlMessage = htmlMessage.Replace("{{Username}}", emailData.UserName);
+            htmlMessage = htmlMessage.Replace("{{ResetLink}}", emailData.ResetLink);
+
+            var msg = new SendGridMessage()
+            {
+                From = new EmailAddress(Options.SendGridSenderEmail, Options.SendGridSenderName + " Password Recovery"),
+                Subject = subject,
+                HtmlContent = htmlMessage
+            };
+            msg.AddTo(new EmailAddress(toEmail));
+
+            // Disable click tracking.
+            // See https://sendgrid.com/docs/User_Guide/Settings/tracking.html
+
+            msg.SetClickTracking(false, false);
+            var response = await client.SendEmailAsync(msg);
+            _logger.LogInformation(response.IsSuccessStatusCode
+                                   ? $"Email to {toEmail} queued successfully!"
+                                   : $"Failure Email to {toEmail}");
+        }
+    }
+}
---
+++ backend/TinderForPets_Back/TinderForPets.Infrastructure/AuthMessageSenderOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace TinderForPets.Infrastructure
+{
+    public class AuthMessageSenderOptions
+    {
+        public string? SendGridKey { get; set; }
+        public string? SendGridSenderEmail { get; set; }
+        public string? SendGridSenderName { get; set; }
+    }
+}
