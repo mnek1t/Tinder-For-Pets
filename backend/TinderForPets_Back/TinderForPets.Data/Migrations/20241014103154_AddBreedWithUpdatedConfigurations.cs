58f4644cbbfabf4da2a08bf72fec951e3f4b4ee6
mnek1t <mednikita2004@gmail.com>
2024-10-14 13:37:29 +0300

    applying breed table to db

---
+++ backend/TinderForPets_Back/TinderForPets.Data/Migrations/20241014103154_AddBreedWithUpdatedConfigurations.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace TinderForPets.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBreedWithUpdatedConfigurations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "age",
+                table: "animal");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "sex_id",
+                table: "animal_profile",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "animal_profile",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "animal_id",
+                table: "animal_profile",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "age",
+                table: "animal_profile",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "breed",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    animal_type_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("breed_pkey", x => x.Id);
+                    table.ForeignKey(
+                        name: "animal_breed_type_id_fkey",
+                        column: x => x.animal_type_id,
+                        principalTable: "animal_type",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_breed_animal_type_id",
+                table: "breed",
+                column: "animal_type_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "breed");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "sex_id",
+                table: "animal_profile",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "animal_profile",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "animal_id",
+                table: "animal_profile",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "age",
+                table: "animal_profile",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddColumn<int>(
+                name: "age",
+                table: "animal",
+                type: "integer",
+                nullable: true);
+        }
+    }
+}
