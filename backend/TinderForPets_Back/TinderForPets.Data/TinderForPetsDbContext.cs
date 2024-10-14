3ce17f7d5b6dfa3dbaa6dd44c4625005d753c7a2
mnek1t <mednikita2004@gmail.com>
2024-10-14 13:37:09 +0300

    refreshing context

--- backend/TinderForPets_Back/TinderForPets.Data/TinderForPetsDbContext.cs
+++ backend/TinderForPets_Back/TinderForPets.Data/TinderForPetsDbContext.cs
@@ -19,7 +19,7 @@ public partial class TinderForPetsDbContext : DbContext
     public virtual DbSet<UserAccount> UserAccounts { get; set; }
     public virtual DbSet<Role> Roles { get; set; }
     public virtual DbSet<UserRole> UserRoles { get; set; }
-
+    public virtual DbSet<Breed> Breeds { get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseNpgsql("Host=localhost;Database=tinder_for_pets;Username=mykyta;Password=Tinder123!");
@@ -30,6 +30,7 @@ public partial class TinderForPetsDbContext : DbContext
         modelBuilder.ApplyConfiguration(new AnimalProfileConfiguration());
         modelBuilder.ApplyConfiguration(new AnimalImageConfiguration());
         modelBuilder.ApplyConfiguration(new AnimalTypeConfiguration());
+        modelBuilder.ApplyConfiguration(new BreedConfiguration());
         modelBuilder.ApplyConfiguration(new RoleConfiguration());
         modelBuilder.ApplyConfiguration(new SexConfiguration());
         modelBuilder.ApplyConfiguration(new UserAccountConfiguration());
