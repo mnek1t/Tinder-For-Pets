edc2af12ac561c9a5bbbefb934b131955c381ce0
mnek1t <mednikita2004@gmail.com>
2024-10-17 16:03:07 +0300

    Creating DTOs for fronend

---
+++ backend/TinderForPets_Back/TinderForPets.Application/DTOs/AnimalTypeDto.cs
@@ -0,0 +1,8 @@
+﻿namespace TinderForPets.Application.DTOs
+{
+    public class AnimalTypeDto 
+    {
+        public int Id { get; set; }
+        public string TypeName { get; set; } = string.Empty;
+    }
+}
