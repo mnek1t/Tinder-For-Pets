5233866cf4c106a5f246d69864df2785ca618011
mnek1t <mednikita2004@gmail.com>
2024-10-17 16:00:56 +0300

    interfaces of repositories for data entities

---
+++ backend/TinderForPets_Back/TinderForPets.Data/Interfaces/IAnimalTypeRepository.cs
@@ -0,0 +1,15 @@
+﻿using SharedKernel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TinderForPets.Data.Entities;
+
+namespace TinderForPets.Data.Interfaces
+{
+    public interface IAnimalTypeRepository
+    {
+        Task<Result<List<AnimalType>>> GetAllAnimalTypesAsync();
+    }
+}
