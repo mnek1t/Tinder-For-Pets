ed33cf41c7a81b9b1154500e2820ce118e7b3971
mnek1t <mednikita2004@gmail.com>
2024-10-19 18:00:49 +0300

    add new columns to  contracts

--- backend/TinderForPets_Back/TinderForPets.API/Contracts/Profiles/CreateAnimalProfileRequest.cs
+++ backend/TinderForPets_Back/TinderForPets.API/Contracts/Profiles/CreateAnimalProfileRequest.cs
@@ -11,6 +11,10 @@ namespace TinderForPets.API.Contracts.Profiles
         [Required] bool IsVaccinated,
         [Required] bool IsSterilized,
         [Required] int BreedId,
-        [Required] Guid OwnerId // in the top of my head, we need to take this from cookie where jwt token is located. jwt token includes encrypted userId
+        [Required] Guid OwnerId,// in the top of my head, we need to take this from cookie where jwt token is located. jwt token includes encrypted userId
+        [Required] string Country,
+        [Required] string City,
+        [Required] decimal Height,
+        [Required] decimal Width
         );
 }
---
+++ backend/TinderForPets_Back/TinderForPets.API/Contracts/Profiles/UpdateAnimalProfileRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TinderForPets.API.Contracts.Profiles
+{
+    public record UpdateAnimalProfileRequest(
+        [Required] string Name,
+        [Required] int TypeId,
+            string Description,
+        [Required] int Age,
+        [Required] int SexId,
+        [Required] bool IsVaccinated,
+        [Required] bool IsSterilized,
+        [Required] int BreedId
+        );
+}
