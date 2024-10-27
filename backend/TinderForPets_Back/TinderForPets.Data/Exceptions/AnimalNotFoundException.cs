20d1664d7ba3e8ef173ddf5ff9de0f597d06230f
mnek1t <mednikita2004@gmail.com>
2024-10-27 17:27:19 +0200

    animal profle updates

---
+++ backend/TinderForPets_Back/TinderForPets.Data/Exceptions/AnimalNotFoundException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinderForPets.Data.Exceptions
+{
+    public class AnimalNotFoundException : Exception
+    {
+        public AnimalNotFoundException()
+        : base($"Animal was not found.") { }
+
+        public AnimalNotFoundException(Guid animalId)
+        : base($"Animal with id '{animalId}' was not found.") { }
+    }
+}
