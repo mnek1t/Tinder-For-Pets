0cb35408834a75e7ef311103f3e5675fffb4aefd
mnek1t <mednikita2004@gmail.com>
2024-11-02 17:59:57 +0200

    handle success sending email

--- frontend/my-app/src/components/ResetPasswordForm.tsx
+++ frontend/my-app/src/components/ResetPasswordForm.tsx
@@ -3,6 +3,7 @@ import {ResetPasswordCredentials} from '../api/authApi';
 import "../styles/form.css"
 import { resetPassword } from '../api/authApi';
 import { useLocation } from 'react-router-dom';
+import ConfirmedWindow from './ConfirmedWindow';
 export default function ResetPasswordForm() {
     const location = useLocation();
     const queryParams = new URLSearchParams(location.search);
@@ -21,14 +22,28 @@ export default function ResetPasswordForm() {
         confirmPassword: "",
         token: ""
     });    
+    
+    const [resetResult, setResetResult] = useState({
+        isSuccess: false,
+        message: ""
+    });
 
     function handleResetPassword(e: React.FormEvent<HTMLFormElement>) {
         e.preventDefault();
         console.log(resetPasswordCreds)
-        resetPassword(resetPasswordCreds);
+        resetPassword(resetPasswordCreds)
+        .then((data) => {
+            setResetResult(prevState => {
+                return({...prevState, message:data, isSuccess : true});
+            })
+        })
+        .catch((error) => {
+            setResetResult(prevState => {
+                return({...prevState, message:error, isSuccess : false});
+            })
+        })
     }
     
-
     function handleInput(e: React.ChangeEvent<HTMLInputElement>) {
         const {name, value} = e.target;
         setResetPasswordCreds(prevState => {
@@ -39,17 +54,20 @@ export default function ResetPasswordForm() {
     return(
         <form className='reset-password' onSubmit={(e) => handleResetPassword(e)}>
             <div className='reset-password-container'>
-                <h1 className="reset-password__header">Reset your password</h1>
-                <p className="forgot-password-hint">Enter new password and confirm it</p>
-                <input type="password" className="reset-password__input" placeholder="Password" value={resetPasswordCreds.newPassword}  name="newPassword" onChange={handleInput} required></input>
-                <input type="password" className="reset-password__input" placeholder="Confirm password" value={resetPasswordCreds.confirmPassword}  name="confirmPassword" onChange={handleInput} required></input>
-                <button className="reset-password__button" type='submit'>Reset Password</button>
-                <br/>
-                <div className="or-alt">
-                    <div className="line"></div>
-                    <div className="text">OR</div>
-                    <div className="line"></div>
-                </div>
+                {resetResult.isSuccess ? (<ConfirmedWindow title={resetResult.message} message="Click <Back to login> to log in with new password" />) : 
+                (<>
+                    <h1 className="reset-password__header">Reset your password</h1>
+                    <p className="forgot-password-hint">Enter new password and confirm it</p>
+                    <input type="password" className="reset-password__input" placeholder="Password" value={resetPasswordCreds.newPassword}  name="newPassword" onChange={handleInput} required></input>
+                    <input type="password" className="reset-password__input" placeholder="Confirm password" value={resetPasswordCreds.confirmPassword}  name="confirmPassword" onChange={handleInput} required></input>
+                    <button className="reset-password__button" type='submit'>Reset Password</button>
+                    <br/>
+                    <div className="or-alt">
+                        <div className="line"></div>
+                        <div className="text">OR</div>
+                        <div className="line"></div>
+                    </div>
+                </>)}
             </div>
             <div className="back-2-login-container">
                 <a className="back-2-login-link" href="/login">Back to login</a>
