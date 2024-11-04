fd8389a34c8fb84e1b825d1cd3d9bf88a3c907d5
mnek1t <mednikita2004@gmail.com>
2024-11-04 15:27:59 +0200

    get animal image repository

--- backend/TinderForPets_Back/TinderForPets.Data/Repositories/AnimalImageRepository.cs
+++ backend/TinderForPets_Back/TinderForPets.Data/Repositories/AnimalImageRepository.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 using TinderForPets.Core.Models;
 using TinderForPets.Data.Entities;
 using TinderForPets.Data.Interfaces;
@@ -7,18 +8,29 @@ namespace TinderForPets.Data.Repositories
 {
     public class AnimalImageRepository : IAnimalImageRepository
     {
-        private readonly TinderForPetsDbContext _dbContext;
+        private readonly TinderForPetsDbContext _context;
         private readonly IMapper _mapper;
         public AnimalImageRepository(TinderForPetsDbContext dbContext, IMapper mapper)
         {
-            _dbContext = dbContext;
+            _context = dbContext;
             _mapper = mapper;
         }
+
+        public async Task<AnimalImage> GetAnimalImageAsync(Guid animalProfileId)
+        {
+            var image = await _context.AnimalImage
+                .Where(ai => ai.AnimalProfileId == animalProfileId)
+                .Select(a => new AnimalImage() { ImageData = a.ImageData, ImageFormat = a.ImageFormat})
+                .FirstOrDefaultAsync();
+
+            return image;
+        }
+
         public async Task<List<AnimalImage>> SaveAnimalMediaAsync(IEnumerable<AnimalImageModel> animalImageModels)
         {
             var animalImageEntities = animalImageModels.Select(am => _mapper.Map<AnimalImage>(am)).ToList();
-            await _dbContext.AddRangeAsync(animalImageEntities);
-            await _dbContext.SaveChangesAsync();
+            await _context.AddRangeAsync(animalImageEntities);
+            await _context.SaveChangesAsync();
             return animalImageEntities;
         }
     }
