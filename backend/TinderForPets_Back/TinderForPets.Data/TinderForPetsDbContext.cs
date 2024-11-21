022419dbde93072ff4c577a8297a6fc7cd04dd2d
mnek1t <mednikita2004@gmail.com>
2024-11-21 16:47:25 +0200

    removing connection string that caused overriding of appSetting file

--- backend/TinderForPets_Back/TinderForPets.Data/TinderForPetsDbContext.cs
+++ backend/TinderForPets_Back/TinderForPets.Data/TinderForPetsDbContext.cs
@@ -21,10 +21,6 @@ public partial class TinderForPetsDbContext : DbContext
     public virtual DbSet<UserRole> UserRoles { get; set; }
     public virtual DbSet<Breed> Breeds { get; set; }
     public virtual DbSet<Match> Matches { get; set; }
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=localhost;Database=tinder_for_pets;Username=mykyta;Password=Tinder123!");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfiguration(new AnimalConfiguration());
