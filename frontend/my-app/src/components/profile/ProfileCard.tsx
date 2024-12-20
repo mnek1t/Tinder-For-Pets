4a3ecbaef65e9b975fe09ef8e426f7b42e680124
mnek1t <mednikita2004@gmail.com>
2024-12-20 15:38:55 +0200

    move main functionality to parent component

--- frontend/my-app/src/components/profile/ProfileCard.tsx
+++ frontend/my-app/src/components/profile/ProfileCard.tsx
@@ -1,58 +1,52 @@
 import { getProfileDetails, ProfileDetailsData } from "../../api/profileApi";
-import { useState, useEffect } from "react";
-export default function ProfileCard()
-{
-    const [profileDetails, setProfileDetails] = useState<ProfileDetailsData | null>(null);
-    const [isDataReceived, setIsDataReceived] = useState<boolean>(false);
-    useEffect(() => {
-        getProfileDetails()
-        .then(({ profile, animal, images }) => {
-            setProfileDetails({
-                animal: {
-                  type: animal?.animalType || "", // Ensure the animal type is set properly
-                  breed: animal?.breed || "", // Ensure breed is set properly
-                },
-                profile: {
-                  id: profile?.id || "", // Ensure the profile id is set
-                  name: profile?.name || "",
-                  description: profile?.description || "",
-                  age: profile?.age || 0,
-                  sex: profile?.sex || "",
-                  isVaccinated: profile?.isVaccinated || false,
-                  isSterilized: profile?.isSterilized || false,
-                },
-                images: [
-                  {
-                    imageData: images[0]?.imageData || "", // Safely access imageData
-                    imageFormat: images[0]?.imageFormat || "", // Safely access imageFormat
-                  },
-                ],
-              });
-            setIsDataReceived(true);
-        })
-        .catch(error => {
-            setIsDataReceived(false);
-            console.error('Error fetching profile details:', error);
-        })
-    }, [])
+import React, { useState, useEffect } from "react";
+import { CircularProgress, FormControlLabel } from '@mui/material';
+import CustomSwitch from "../../CustomSwitch";
+interface ProfileCardProps {
+  handleClickEditProfileCard : (event: React.MouseEvent<HTMLButtonElement>) => void;
+  profileDetails : ProfileDetailsData | null;
+  loading: boolean
+}
 
+export default function ProfileCard(props: ProfileCardProps)
+{
     return(
         <>
-            {isDataReceived && <div className="profile-card">
-                <img className="animal-image" src={`data:${profileDetails?.images[0].imageFormat};base64,${profileDetails?.images[0].imageData}`} alt="Animal"/>
-                <h1 className="profile-card__name">{profileDetails?.profile.name}, {profileDetails?.profile.age}</h1>
-                <p className="profile-card__title">{profileDetails?.animal.type}, {profileDetails?.animal.breed}</p>
-                <p>{profileDetails?.profile.description}</p>
-                <div className="medical-profile-data">
-                    <label htmlFor="is_vaccinated">Is Vacinated :</label>
-                    <input id="is_vaccinated" type="checkbox" checked={profileDetails?.profile.isVaccinated}/>
-                </div>
-                <div className="medical-profile-data">
-                    <label htmlFor="is_sterilized">Is Sterilized : </label>
-                    <input id="is_sterilized" type="checkbox" checked={profileDetails?.profile.isSterilized}/>
-                </div>
-                <button className="edit-profile-btn" onClick={() => console.log("This will call edit profile page")}>Edit Profile</button>
-            </div>
+            {!props.loading ? <div className="profile-card">
+                <img className="animal-image" src={`data:${props.profileDetails?.images[0].imageFormat};base64,${props.profileDetails?.images[0].imageData}`} alt="Animal"/>
+                <h1 className="profile-card__name">{props.profileDetails?.profile.name}, {props.profileDetails?.profile.age}</h1>
+                <p className="profile-card__title">{props.profileDetails?.animal.type}, {props.profileDetails?.animal.breed}</p>
+                <p>{props.profileDetails?.profile.description}</p>
+                <FormControlLabel
+                  control={
+                    <CustomSwitch name="isVaccinated" checked={props.profileDetails?.profile.isVaccinated}/>
+                  }
+                  label="Vacinated:"
+                  labelPlacement="start"
+                  sx={{
+                    fontFamily: "'Karla', sans-serif", 
+                    fontSize: "14px",                 
+                    ".MuiFormControlLabel-label": {
+                      fontFamily: "'Karla', sans-serif", 
+                    },
+                  }}
+                />
+                <FormControlLabel
+                  control={
+                    <CustomSwitch name="isSterilized" checked={props.profileDetails?.profile.isSterilized}/>
+                  }
+                  label="Sterialized:"
+                  labelPlacement="start"
+                  sx={{
+                    fontFamily: "'Karla', sans-serif", 
+                    fontSize: "14px",                  
+                    ".MuiFormControlLabel-label": {
+                      fontFamily: "'Karla', sans-serif", 
+                    },
+                  }}
+                />
+                <button className="edit-profile-btn" onClick={props.handleClickEditProfileCard}>Edit Profile</button>
+            </div> : <CircularProgress sx={{color: "#864c4c" }} size={48} />
         }
         </>
         
--- frontend/my-app/src/pages/ProfilePage.tsx
+++ frontend/my-app/src/pages/ProfilePage.tsx
@@ -1,25 +1,76 @@
 
 // TODO: IMPLEMENT GET DATA IN PAGE AND IF ERROR DO NOT RENDER ANYTHING
+import React, { useState, useEffect } from "react";
 import Sidebar from "../components/Sidebar";
 import ProfileCard from "../components/profile/ProfileCard";
 import FormPageWrapper from '../components/FormPageWrapper';
-import { deleteAccount } from "../api/authApi";
-import { logout } from "../api/authApi"
-import { useNavigate } from "react-router-dom";
-import React, { useState, useEffect } from "react";
 import ConfirmationPopup from "../components/ConfirmationPopup";
+import ProfileEditForm from "../components/ProfileEditForm";
+import { logout, deleteAccount } from "../api/authApi";
+import { getProfileDetails, ProfileDetailsData, getAnimalTypes, getBreeds } from "../api/profileApi";
+import { useNavigate } from "react-router-dom";
+import { CircularProgress } from "@mui/material";
+import { DEFAULT_ANIMAL_TYPE_ID } from "../utils/TinderConstants";
 
 const deleteAccountDescription = "If you delete your account, you will permanently lose your profile, messages, photos and matches. If you delete your account, this action cannot be undone."
 
 export default function ProfilePage() {
+    const [animalTypes, setAnimalTypes] = useState([]);
+    const [breeds, setBreeds] = useState([]);
     const [distance, setDistance] = useState(50);
     const [showPetsInRange, setShowPetsInRange] = useState(true);
     const [showConfirmationWindow, setShowConfirmationWindow ] = useState<boolean>(false);
+    const [profileDetails, setProfileDetails] = useState<ProfileDetailsData | null>(null);
+    const [loading, setLoading] = useState(false);
+    const [isEditing, setIsEditing] = useState(false);
     const navigate = useNavigate();
-    
+
+    useEffect(() => {
+            getAnimalTypes()
+              .then((animalTypes) => {
+                setAnimalTypes(animalTypes);
+                handleFetchBreeds(animalTypes[0]?.id || profileDetails?.animal.type);
+            })
+            .catch((err) => console.error(err));
+        }, []);
+    const handleFetchBreeds = (typeId: number) => {
+        getBreeds(typeId)
+            .then(setBreeds)
+            .catch((err) => console.error(err));
+    };
     useEffect(() => {
         document.body.style.background = '#FEFAF6';
-
+        setLoading(true);
+            getProfileDetails()
+            .then(({ profile, animal, images }) => {
+            setProfileDetails({
+                    animal: {
+                    type: animal?.animalType || "", 
+                    breed: animal?.breed || "", 
+                    },
+                    profile: {
+                    id: profile?.id || "", 
+                    name: profile?.name || "",
+                    description: profile?.description || "",
+                    age: profile?.age || 0,
+                    sex: profile?.sex || "",
+                    isVaccinated: profile?.isVaccinated || false,
+                    isSterilized: profile?.isSterilized || false,
+                    },
+                    images: [
+                    {
+                        imageData: images[0]?.imageData || "", 
+                        imageFormat: images[0]?.imageFormat || "", 
+                    },
+                    ],
+                });
+            })
+            .catch(error => {
+                console.error('Error fetching profile details:', error);
+            })
+            .finally(() => {
+                setLoading(false);
+            })
         return () => {
             document.body.style.backgroundImage = "";
             document.body.style.backgroundSize = '';
@@ -27,8 +78,16 @@ export default function ProfilePage() {
     }, []);
     
     function handleLogout(event : React.MouseEvent<HTMLButtonElement>) {
-        logout();
-        navigate("/about");
+        setLoading(true);
+        logout()
+        .then(() => {
+            navigate("/about");
+        })
+        .catch((error) => {
+            console.error(error);
+        })
+        .finally(() => setLoading(false));
+        
     }
 
     function handleDeleteAccountButtonClick(event : React.MouseEvent<HTMLButtonElement>) {
@@ -59,30 +118,43 @@ export default function ProfilePage() {
         }
     }
 
+    function handleClickEditProfileCard(event : React.MouseEvent<HTMLButtonElement> ) {
+        setIsEditing((prev) => !prev)
+    }
+
     return(
         <>
             <div className="container">
-                <div className="sidebar-container">
-                    <Sidebar 
-                        isUserPreferences = {true}
-                        handleLogout={handleLogout} 
-                        handleDeleteAccount={handleDeleteAccountButtonClick} 
-                        distance={distance}
-                        onDistanceChange={setDistance}
-                        showPetsInRange={showPetsInRange}
-                        onToggleShowPets={toggleShowPets}
-                        onLightMode={enableLightMode}
-                        onDarkMode={enableDarkMode}/>
-                </div>
-                
-                <div className="profile">
-                    <ProfileCard />
-                </div>
-                {
-                    showConfirmationWindow && 
+                {loading ? (
                     <FormPageWrapper title="delete-account" showHeader={false}>
-                        <ConfirmationPopup question="You sure you want to delete an account?" description={deleteAccountDescription} handleAction={handleDeleteAccount}/> 
-                    </FormPageWrapper>
+                        <CircularProgress/> 
+                    </FormPageWrapper>) :
+                    (
+                        <>
+                            <div className="sidebar-container">
+                                <Sidebar 
+                                    isUserPreferences = {true}
+                                    handleLogout={handleLogout} 
+                                    handleDeleteAccount={handleDeleteAccountButtonClick} 
+                                    distance={distance}
+                                    onDistanceChange={setDistance}
+                                    showPetsInRange={showPetsInRange}
+                                    onToggleShowPets={toggleShowPets}
+                                    onLightMode={enableLightMode}
+                                    onDarkMode={enableDarkMode}/>
+                            </div>
+                            
+                            <div className="profile">
+                                {isEditing ? <ProfileEditForm animalTypes={animalTypes} breeds={breeds} profileDetails={profileDetails} handleClickEditProfileCard={handleClickEditProfileCard}/> : <ProfileCard profileDetails={profileDetails} loading={loading} handleClickEditProfileCard={handleClickEditProfileCard}/>}
+                            </div>
+                            {
+                                showConfirmationWindow && 
+                                <FormPageWrapper title="delete-account" showHeader={false}>
+                                    <ConfirmationPopup question="You sure you want to delete an account?" description={deleteAccountDescription} handleAction={handleDeleteAccount}/> 
+                                </FormPageWrapper>
+                            }
+                        </>
+                    )
                 }
             </div>       
             {/* {isDataReceived && <div className="container">
