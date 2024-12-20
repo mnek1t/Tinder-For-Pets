f01e547abb95d5becc9c7ce619ed3fb7b48ca8f2
mnek1t <mednikita2004@gmail.com>
2024-12-20 10:58:42 +0200

    move create profile functionality to parent component

--- frontend/my-app/src/components/profile/CreateProfileForm.tsx
+++ frontend/my-app/src/components/profile/CreateProfileForm.tsx
@@ -1,123 +1,49 @@
 import "../../styles/form.css";
-import { getAnimalTypes, getBreeds, getSexes, ProfileData} from "../../api/profileApi";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import LocationSuggestion from "./LocationSuggestion"; 
-
+import { ProfileData } from "../../api/profileApi";
+import Error from "./Error";
+import LoadButton from "../LoadButton";
+import { FormGroup, FormControlLabel } from "@mui/material";
+import CustomSwitch from "../../CustomSwitch";
+import FileUploader from "../FileUploader";
+import { DEFAULT_SEX_ID, DEFAULT_ANIMAL_TYPE_ID, DEFAULT_BREED_ID } from "../../utils/TinderConstants";
 interface CreateProfileProps {
-    createProfile : (event: React.FormEvent<HTMLFormElement>, data : ProfileData) => void
-}
-
-interface AnimalType {
-    id: number,
-    typeName: string;
-}
-
-interface Breed {
-    id: number,
-    breedName: string;
-}
-
-interface Sex {
-    id: number,
-    sexName: string;
+    animalTypes: Array<{ id: number; typeName: string }>;
+    sexes: Array<{ id: number; sexName: string }>;
+    breeds: Array<{ id: number; breedName: string }>;
+    error?: Error | null;
+    loading: boolean,
+    handleCreateProfile : (petProfileData : ProfileData) => void,
+    handleFetchBreeds : (animalTypeId : number) => void
 }
 
 export default function CreateProfileForm(props : CreateProfileProps) {
-    const [animalTypes, setAnimalTypes] = useState<JSX.Element[]>([]);
-    const [breeds, setBreeds] = useState<JSX.Element[]>([]);
-    const [sexes, setSexes] = useState<JSX.Element[]>([]);
-    const [selectedSex, setSelectedSex] = useState<number>(1);
-    const [selectedAnimalType, setSelectedAnimalType] = useState<number>(1);
-    const [selectedBreed, setSelectedBreed] = useState<number>(98);
     const [profileData, setProfileData] = useState<ProfileData>({
         name: "",
         description: "",
-        typeId: selectedAnimalType,
-        breedId: selectedBreed,
-        // files: undefined,
+        typeId: props.animalTypes[0]?.id || DEFAULT_ANIMAL_TYPE_ID,
+        breedId: props.breeds[0]?.id || DEFAULT_BREED_ID,
         dateOfBirth: "",
-        sexId: selectedSex,
+        sexId: props.sexes[0]?.id || DEFAULT_SEX_ID,
         isVaccinated: true,
         isSterilized: true,
         country: "",
         city: "",
+        file: null,
         height: 0,
-        weight: 0
+        weight: 0,
     });
-   
-    useEffect(() => {
-        getAnimalTypes()
-        .then((data) => {
-            const elements = data.map((type: AnimalType) => {
-                return(
-                    <option className="animal-type" key={type.id} value={type.id}>
-                         {type.typeName.charAt(0).toUpperCase() + type.typeName.slice(1)}
-                    </option>
-                );
-            })
-            setAnimalTypes(elements);
-        }).catch(error => {
-            console.error(error);
-        });
-    }, [])
 
-    useEffect(() => {
-        getSexes()
-        .then((data) => {
-            const elements = data.map((sex : Sex) => {
-                return(
-                    <button name="sexId" key={sex.id} value={sex.id} className={`gender-button ${selectedSex === sex.id ? "selected" : ""}`} onClick={handleClick}>
-                        {sex.sexName}
-                    </button>
-                );
-            });
-
-            setSexes(elements);
-        })
-    }, [selectedSex])
-
-    useEffect(() => {
-        getBreeds(selectedAnimalType)
-        .then((data) => {
-            const elements = data.map((breed: Breed) => {
-                return(
-                    <option className="breed" key={breed.id} value={breed.id}>
-                         {breed.breedName.charAt(0).toUpperCase() + breed.breedName.slice(1)}
-                    </option>
-                );
-            })
-            setBreeds(elements);
-        }).catch(error => {
-            console.error(error);
-        })
-    }, [selectedAnimalType]);
-
-    function handleInput(event : React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement> | React.ChangeEvent<HTMLTextAreaElement>) {
-        const {name, value} = event.target;
-        if(name === 'typeId') {
-            console.log('type id works')
-            setSelectedAnimalType(Number(value));
-            setSelectedBreed(Number(value) === 1 ? 98 : 1);
-            setProfileData(prevState => {
-                return {...prevState, "breedId": Number(value) === 1 ? 98 : 1}
-            })
-        }
-
-        if(name === 'breedId') {
-            setSelectedBreed(Number(value));
-        }
-
-        if(isNaN(Number(value))) {
-            setProfileData(prevState => {
-                return {...prevState, [name]: value}
-            })
-        }
-        else {
-            setProfileData(prevState => {
-                return {...prevState, [name]: Number(value)}
-            })
-        }
-    }
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => {
+        const { name, value } = e.target;
+        setProfileData((prev) => ({
+          ...prev,
+          [name]: isNaN(Number(value)) ? value : Number(value),
+        }));
+    
+        if (name === "typeId") props.handleFetchBreeds(Number(value));
+      };
 
     function handleImageUpload(event : React.ChangeEvent<HTMLInputElement>) {
         const {name, files} = event.target;
@@ -129,18 +55,13 @@ export default function CreateProfileForm(props : CreateProfileProps) {
         }
     }
 
-    function handleClick(event : React.MouseEvent<HTMLButtonElement>) {
-        event.preventDefault();
-        const {name, value} = event.target as HTMLButtonElement;
-        if (name === "sexId") {
-            setSelectedSex(Number(value));
-        }
-
-        setProfileData(prevState => {
-            return {...prevState, [name]: Number(value)}
-        })
-        
-    }
+    const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, checked } = e.target;
+        setProfileData((prev) => ({
+          ...prev,
+          [name]: checked,
+        }));
+    };
 
     const handleLocationSelect = (city: string, country: string) => {
         setProfileData(prevState => ({
@@ -150,94 +71,137 @@ export default function CreateProfileForm(props : CreateProfileProps) {
         }));
     };
 
-    const handleCheck = (event : React.ChangeEvent<HTMLInputElement>) => {
-        const {name, checked} = event.target;
-
-        setProfileData(prevState => {
-            return {...prevState, [name]: checked}
-        })
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        props.handleCreateProfile(profileData);
     };
-    console.log(profileData)
+
     return(
-        <form className="create-profile-form" onSubmit={(e) => props.createProfile(e, profileData)}>
+        <form className="create-profile-form" onSubmit={handleSubmit}>
             <h1 className="create-profile__header">Create profile</h1>
+            {props.error && <Error error={props.error}/>}
             <div className="grid">
                 <div>
                     <div>
-                        <label htmlFor="name-input">Name</label>
-                        <input value={profileData.name} id="name-input" placeholder="Pet name" name="name" onChange={handleInput}></input>  
+                        <label htmlFor="name">Name</label>
+                        <input className="create-profile-form__name" value={profileData.name} placeholder="Pet name" name="name" onChange={handleInputChange} required></input>  
                     </div>
-                    <div>
-                        <label htmlFor="description-input">About pet</label>
-                        <textarea id="description-input" name="description" value={profileData.description} placeholder="Tell about your pet" onChange={handleInput}></textarea>  
+
+                    <div style={{height:'26%'}}>
+                        <label htmlFor="description">About pet</label>
+                        <textarea 
+                            name="description"
+                            defaultValue=''
+                            className="create-profile-form__description"
+                            value={profileData.description} 
+                            placeholder="Tell about your pet" 
+                            onChange={handleInputChange}>
+                        </textarea>  
                     </div>
+
                     <div>
-                        <label htmlFor="animal-type-dropbox">Animal Type</label>
-                        <select name="typeId" id="animal-type-dropbox" onChange={handleInput}>
-                            {animalTypes}
+                        <label htmlFor="typeId">Animal Type</label>
+                        <select name="typeId" className="create-profile-form__animal-type" onChange={handleInputChange} required>
+                        {props.animalTypes.map((type) => (
+                            <option key={type.id} value={type.id}>
+                                {String(type.typeName).charAt(0).toUpperCase() + String(type.typeName).slice(1)}
+                            </option>
+                        ))}
                         </select>
                     </div>
                     <div>
-                        <label htmlFor="breeds-dropbox">Breed</label>
-                        <select name="breedId" id="breeds-dropbox" onChange={handleInput}>
-                            {breeds}
+                        <label htmlFor="breedId">Breed</label>
+                        <select name="breedId" className="create-profile-form__breed" onChange={handleInputChange} required>
+                            {props.breeds.map((breed) => (
+                                <option key={breed.id} value={breed.id}>
+                                    {breed.breedName}
+                                </option>
+                            ))}
                         </select>
                     </div>
-                    <div>
-                        <label htmlFor="photo-upload">Upload photo</label>
-                        <input type="file" accept="image/*" aria-label="Name" id="photo-upload" placeholder="Pet name" name="files" onChange={handleImageUpload}></input>  
-                    </div>
 
-                </div>
-                <div>
-                    <div>
-                        <label htmlFor="date-of-birth-dropbox">Date of birth</label>
-                        <input id="date-of-birth-dropbox" type="date" name="dateOfBirth" min="2000-01-01" max="2017-04-30" value={profileData.dateOfBirth} onChange={handleInput}/>
-                    </div>
-
-                    <div>
-                        <label>Select a pet gender:</label>
-                        {sexes}
-                    </div>
-
-                    <div>
-                        <span className="checkbox-inputs">
-                            <span className="checkbox-group">
-                                <label>Is Vaccinated?</label>
-                                <input type="checkbox" name="isVaccinated" checked={profileData.isVaccinated} onChange={handleCheck}/>
-                            </span>
-                            <span className="checkbox-group">
-                                <label>Is Sterilized?</label>
-                                <input type="checkbox" name="isSterilized" checked={profileData.isSterilized} onChange={handleCheck}/>
-                            </span>
-                        </span>
-                    </div>
-
-                    <div>
-                        <LocationSuggestion onSelectLocation={handleLocationSelect}/>
-                    </div>
-                   
+                    <LocationSuggestion onSelectLocation={handleLocationSelect}/>
                     <div className="size-input-group">
-                        <label htmlFor="data-input">Height</label>
+                        <label htmlFor="height">Height</label>
                         <div className="size-input-container">
                             <span className="unit-label">cm</span>
-                            <input id="data-input" className="height-input" min="30" step="0.5" name="height" type="number" value={profileData.height} onChange={handleInput}/>
+                            <input name="height" className="height-input" min="30" step="0.5" type="number" value={profileData.height} onChange={handleInputChange}/>
                         </div>
                     </div>
 
                     <div className="size-input-group">
-                        <label htmlFor="data-input">Weight</label>
+                        <label htmlFor="weight">Weight</label>
                         <div className="size-input-container">
                             <span className="unit-label">kg</span>
-                            <input id="data-input" className="weight-input" min="0" step="0.2" name="weight" type="number" value={profileData.weight} onChange={handleInput}/>
+                            <input name="weight" className="weight-input" min="0" step="0.2"  type="number" value={profileData.weight} onChange={handleInputChange}/>
+                        </div>
+                    </div>
+                </div>
+
+                <div>
+                    <div>
+                        <label htmlFor="dateOfBirth">Date of birth:</label>
+                        <input name="dateOfBirth" className="create-profile-form__date-of-birth" type="date" min="2000-01-01" max="2017-04-30" value={profileData.dateOfBirth} onChange={handleInputChange} required/>
+                    </div>
+                    <div style={{height:'26%'}}>
+                        <div>
+                            <label htmlFor="sexId">Pet gender:</label>
+                            <div className="create-profile-form__gender-buttons-container">
+                                {props.sexes.map((sex) => (
+                                    <button
+                                        key={sex.id}
+                                        type="button"
+                                        name="sexId"
+                                        value={sex.id}
+                                        className= {profileData.sexId === sex.id ? "create-profile-form__gender-button selected" : "create-profile-form__gender-button"}
+                                        onClick={(e) => handleInputChange(e as any)}>
+                                        {sex.sexName}
+                                    </button>
+                                ))}
+                            </div>
+                            
+                        </div>
+
+                        <div>
+                            <FormGroup aria-label="position" row>
+                                <FormControlLabel
+                                    control={
+                                        <CustomSwitch name="isVaccinated" checked={profileData.isVaccinated} onChange={handleCheckboxChange}/>
+                                    }
+                                    label="Is Vaccinated?"
+                                    sx={{
+                                        fontFamily: "'Karla', sans-serif", 
+                                        fontSize: "14px",                  
+                                        ".MuiFormControlLabel-label": {
+                                          fontFamily: "'Karla', sans-serif", 
+                                        },
+                                      }}
+                                />
+                                <FormControlLabel
+                                    control={
+                                        <CustomSwitch name="isSterilized" checked={profileData.isSterilized} onChange={handleCheckboxChange}/>
+                                    }
+                                    label="Is Sterilized?"
+                                    sx={{
+                                        fontFamily: "'Karla', sans-serif", 
+                                        fontSize: "14px",                  
+                                        ".MuiFormControlLabel-label": {
+                                          fontFamily: "'Karla', sans-serif", 
+                                        },
+                                      }}
+                                />
+                            </FormGroup>
                         </div>
                     </div>
 
                     <div>
-                        <button className="create-profile-btn" type="submit">Create</button>
+                        <label htmlFor="photo-upload">Upload photo:</label>
+                        <FileUploader handleFileUpload={handleImageUpload} maxSizeMB="10" accept="image/*" initialImage={null}/>
                     </div>
                 </div>
             </div>
+            <br/>
+            <LoadButton innertText="Create" loading={props.loading}/>
         </form>
     );
 }
