6c94440d08febad8a54e2327433f1b1f6cb54373
mnek1t <mednikita2004@gmail.com>
2025-02-04 01:00:41 +0200

    extract OperationCancelled error data to separate class

---
+++ backend/TinderForPets_Back/TinderForPets.Core/OperationCancellationErrors.cs
@@ -0,0 +1,15 @@
+﻿using SharedKernel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinderForPets.Core
+{
+    public static class OperationCancellationErrors
+    {
+        public static readonly Error OperationCancelled = new(
+           "Operation.IsCanceled", "The following operation was canceled");
+    }
+}
