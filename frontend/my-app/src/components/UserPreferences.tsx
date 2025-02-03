586bc6aea9f4faba96261f1818f8907d821f2277
mnek1t <mednikita2004@gmail.com>
2025-02-04 01:22:26 +0200

    move jsx to separate child component

--- frontend/my-app/src/components/UserPreferences.tsx
+++ frontend/my-app/src/components/UserPreferences.tsx
@@ -1,14 +1,9 @@
 import "../styles/profile.css";
-import CustomSwitch from "../CustomSwitch";
-import { FormControlLabel, Typography } from "@mui/material";
-import CustomSlider from "./CustomSlider";
+import RecommendationDistanceSlider from "./RecommendationDistanceSlider";
+import AppearenceMode from "./AppearenceMode";
 export interface UserPreferencesProps {
     distance: number;
-    onDistanceChange: (value: number) => void;
-    showPetsInRange: boolean;
     onToggleShowPets: () => void;
-    onLightMode: () => void;
-    onDarkMode: () => void;
     handleLogout: (event : React.MouseEvent<HTMLButtonElement>) => void;
     handleDeleteAccount: (event : React.MouseEvent<HTMLButtonElement>) => void;
   }
@@ -19,40 +14,8 @@ export default function UserPreferences(props : UserPreferencesProps) {
             <h3 className="sidebar-title">ACCOUNT SETTINGS</h3>
             <button className="preference-btn">Manage Payment Account</button>
             <h3 className="sidebar-title">DISCOVERY SETTINGS</h3>
-            <div>
-                <Typography gutterBottom 
-                    sx={{
-                        fontFamily: "'Karla', sans-serif", 
-                        fontSize: "14px",                  
-                        ".MuiFormControlLabel-label": {
-                        fontFamily: "'Karla', sans-serif", 
-                        },
-                    }}>
-                        Distance Preference {props.distance}km
-                </Typography>
-                <CustomSlider aria-label="ios slider" value={props.distance}  valueLabelDisplay="on" onChange={(e, value) => props.onDistanceChange(value as number)}/>
-
-                <div className="toggler-preference">
-                        <FormControlLabel
-                            control={
-                                <CustomSwitch name="isSterilized" checked={props.showPetsInRange} onChange={props.onToggleShowPets}/>
-                            }
-                            label="Only show pets in this range"
-                            labelPlacement="start"
-                            sx={{
-                                fontFamily: "'Karla', sans-serif", 
-                                fontSize: "14px",                  
-                                ".MuiFormControlLabel-label": {
-                                    fontFamily: "'Karla', sans-serif", 
-                                },
-                            }}
-                        />
-                </div>    
-            </div>
-            
-            <h3 className="sidebar-title">APPEARANCE</h3>
-            <button className="preference-btn" onClick={props.onLightMode}>Light Mode</button>
-            <button className="preference-btn" onClick={props.onDarkMode}>DarkMode</button>
+            <RecommendationDistanceSlider/>
+            <AppearenceMode/>
 
             <h3 className="sidebar-title">LEGAL</h3>
             
