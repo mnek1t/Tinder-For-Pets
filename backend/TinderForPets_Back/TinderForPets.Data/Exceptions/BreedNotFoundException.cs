68a6d8521777d109df6cc0ef28f867c03c671289
mnek1t <mednikita2004@gmail.com>
2025-02-04 01:05:04 +0200

    exception for handling breeds

---
+++ backend/TinderForPets_Back/TinderForPets.Data/Exceptions/BreedNotFoundException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinderForPets.Data.Exceptions
+{
+    public class BreedNotFoundException : Exception
+    {
+        public BreedNotFoundException() : base("Breeds not found according to this Amimal Type Id") { }
+    }
+}
